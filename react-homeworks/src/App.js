0efae09c18f3698b881f388334e7ed529bbde386
Yana Kim <ykim.89@mail.ru>
2022-11-10 23:28:37 +0300

    hw-28

---
+++ react-homeworks/src/App.js
@@ -0,0 +1,39 @@
+import PageProduct from "./components/PageProduct/PageProduct";
+
+
+function App() {
+  return (
+    <PageProduct />
+  )
+}
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+        
+//         <p>
+//           У меня получилось! УРА!
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
