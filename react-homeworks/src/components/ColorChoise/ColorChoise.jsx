b395059120217486475758def405c26c050e7db8
Yana Kim <ykim.89@mail.ru>
2022-12-01 21:54:04 +0300

    hw-35

--- react-homeworks/src/components/ColorChoise/ColorChoise.jsx
+++ react-homeworks/src/components/ColorChoise/ColorChoise.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import "./ColorChoise.css";
 import ColorButton from "./ColorButton";
 
@@ -40,6 +40,10 @@ export default function ColorChoise() {
     },
   ];
 
+  const handleClickSelectedBtn = useCallback(() => (item) =>{
+    setSelectedButton(item)
+  }, [selectedButton])
+
   return (
 
     <div className="subsection">
@@ -56,7 +60,7 @@ export default function ColorChoise() {
           return <ColorButton
             key={ img.id }
             className={ className }
-            onClick={ () => { setSelectedButton(img.color) } }
+            onClick={ ( ) => {handleClickSelectedBtn(img.color)} }
             src={ img.src }
           />
         })}
